8f3c607e5eda1754d7dfff8abb9dc62df5378986
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-27 12:25:01 +0700

    addons: Implement Volume Styles [3/3]
    Co-authored-by: DrDisagree <crazymahmud08@gmail.com>

--- config.mk
+++ config.mk
@@ -398,6 +398,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay
 
+# Volume Styles
+PRODUCT_PACKAGES += \
+    VolumeDoubleLayer \
+    VolumeGradient \
+    VolumeNeumorph \
+    VolumeNeumorphOutline \
+    VolumeOutline \
+    VolumeShadedLayer \
+    VolumeAOSPRevamped
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
